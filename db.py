91098b6ad3c4623830f27cf2f497df9dbf23bcc5
LeonidZhirov <assasincreed2005@mail.ru>
2026-06-08 22:09:57 +0200

    refactor(db): change the work with engine and connection from SQLAlchemy Core to session and sessionmaker from SQLAlchemy ORM

--- db.py
+++ db.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:55432/northwind"
 engine = create_engine(DATABASE_URL, echo=True)
 
-def get_connection():
-    return engine.connect()
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+session = SessionLocal()
