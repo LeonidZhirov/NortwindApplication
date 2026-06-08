485759f0b87ec047d512c67af23b4cf402635e0e
LeonidZhirov <assasincreed2005@mail.ru>
2026-06-08 21:56:36 +0200

    refactor(customer_model.py): refactor SQLAlchemy Core elements to SQLAlchemy ORM elements

--- models/customer_model.py
+++ models/customer_model.py
@@ -1,10 +1,17 @@
-from dataclasses import dataclass
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-@dataclass
-class Customer:
-    customer_id: int
-    company_name: str
-    contact_name: str
 
-    def __str__(self):
+class Base(DeclarativeBase):
+    pass
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    customer_id: Mapped[str] = mapped_column(String(10), primary_key=True)
+    company_name: Mapped[str] = mapped_column(String(40), nullable=False)
+    contact_name: Mapped[str] = mapped_column(String(30), nullable=False)
+
+    def __str__(self) -> str:
         return f"{self.customer_id:<10} | {self.company_name:<40} | {self.contact_name:<30}"
