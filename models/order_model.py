126730dac35de29451e0deba06e872ba961bac73
LeonidZhirov <assasincreed2005@mail.ru>
2026-06-09 17:36:06 +0200

    add(order_model): add relationship with Customer, OrderDetail and type checking for Base class

--- models/order_model.py
+++ models/order_model.py
@@ -1,27 +1,46 @@
-from typing import List
+from typing import List, TYPE_CHECKING
 from sqlalchemy import String, Integer, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from models.cart_item_model import CartItem
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from models.base import Base
 
-
-class Base(DeclarativeBase):
-    pass
+if TYPE_CHECKING:
+    from models.order_detail_model import OrderDetail
+    from models.cart_item_model import CartItem
+    from models.customer_model import Customer
 
 
 class Order(Base):
     __tablename__ = 'orders'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    order_id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True
+    )
+
     customer_id: Mapped[
         str] = mapped_column(String(10), ForeignKey('customers.customer_id'), nullable=False, index=True)
     employee_id: Mapped[int] = mapped_column(Integer, ForeignKey('employees.employee_id'), nullable=False)
     ship_via: Mapped[int] = mapped_column(Integer, ForeignKey('shippers.shipper_id'), nullable=False)
 
-    items: Mapped[List["CartItem"]] = relationship(
+    customer: Mapped["Customer"] = relationship(
+        "Customer",
+        back_populates="orders",
+        lazy="selectin"
+    )
+
+    cart_items: Mapped[List["CartItem"]] = relationship(
         "CartItem",
         secondary="order_cart_items",
+        lazy="selectin",
+        back_populates="orders"
+    )
+
+    details: Mapped[List["OrderDetail"]] = relationship(
+        "OrderDetail",
+        back_populates="order",
+        cascade="all, delete-orphan",
         lazy="selectin"
     )
 
     def __str__(self) -> str:
-        return f"Order #{self.id} | {self.customer_id}"
+        return f"Order #{self.order_id} | {self.customer_id}"
