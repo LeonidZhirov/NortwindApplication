914aa68e786eac3f642a8c286f1db05982fc556c
LeonidZhirov <assasincreed2005@mail.ru>
2026-06-09 17:25:28 +0200

    refactor(order_detail_model): change foreign key from orders.id to orders.order_id

--- models/order_detail_model.py
+++ models/order_detail_model.py
@@ -1,25 +1,27 @@
 from decimal import Decimal
+from typing import TYPE_CHECKING
 from sqlalchemy import Integer, ForeignKey, Numeric
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from models.order_model import  Order
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from models.base import Base
 
-
-class Base(DeclarativeBase):
-    pass
-
+if TYPE_CHECKING:
+    from models.order_model import Order
 
 class OrderDetail(Base):
     __tablename__ = 'order_details'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    order_id: Mapped[int] = mapped_column(ForeignKey('orders.id'), nullable=False)
-    product_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    order_id: Mapped[int] = mapped_column(ForeignKey('orders.order_id'), primary_key=True)
+    product_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     unit_price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
     quantity: Mapped[int] = mapped_column(Integer, nullable=False)
     discount: Mapped[float] = mapped_column(Numeric(3, 2), nullable=False, default=0.0)
 
-    # Отношение к заказу
-    order: Mapped["Order"] = relationship("Order", back_populates="items")
+    order: Mapped["Order"] = relationship(
+        "Order",
+        back_populates="details",
+        lazy="selectin"
+    )
+
 
     @property
     def total(self) -> Decimal:
