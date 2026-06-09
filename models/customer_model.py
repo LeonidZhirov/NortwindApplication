74d67c32135b8fa3a194d401e79c3a8da1c647b5
LeonidZhirov <assasincreed2005@mail.ru>
2026-06-09 17:33:57 +0200

    refactor(customer_model.py): import Base class with type checking for work

--- models/customer_model.py
+++ models/customer_model.py
@@ -1,9 +1,10 @@
+from typing import List, TYPE_CHECKING
 from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from models.base import Base
 
-
-class Base(DeclarativeBase):
-    pass
+if TYPE_CHECKING:
+    from models.order_model import Order
 
 
 class Customer(Base):
@@ -12,6 +13,12 @@ class Customer(Base):
     customer_id: Mapped[str] = mapped_column(String(10), primary_key=True)
     company_name: Mapped[str] = mapped_column(String(40), nullable=False)
     contact_name: Mapped[str] = mapped_column(String(30), nullable=False)
+
+    orders: Mapped[List["Order"]] = relationship(
+        "Order",
+        back_populates="customer",
+        lazy="selectin"
+    )
 
     def __str__(self) -> str:
         return f"{self.customer_id:<10} | {self.company_name:<40} | {self.contact_name:<30}"
