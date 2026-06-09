8e73c7927316b7bcbf51189f7ecc3ea417ba8c67
LeonidZhirov <assasincreed2005@mail.ru>
2026-06-09 10:21:27 +0200

    refactor(test_connection): replace SQLAlchemy elements with ORM and change the of derictory to tests

---
+++ tests/test_connection.py
@@ -0,0 +1,18 @@
+from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
+from db import SessionLocal
+
+def test_connection_alternative():
+    session = SessionLocal()
+    try:
+        result = session.execute(text("SELECT 1"))
+        value = result.scalar()
+        print(f"✅ Подключение к БД успешно (результат: {value})")
+        session.commit()
+        return True
+    except OperationalError as e:
+        print(f"❌ Ошибка подключения: {e}")
+        session.rollback()
+        return False
+    finally:
+        session.close()
