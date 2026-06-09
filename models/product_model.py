3ef8ae0c596c4a1a838dbeb452554c650c376c53
LeonidZhirov <assasincreed2005@mail.ru>
2026-06-09 17:10:52 +0200

    refactor(shipper_model): import Base class for work

--- models/product_model.py
+++ models/product_model.py
@@ -1,10 +1,7 @@
 from decimal import Decimal
-from sqlalchemy import Integer, String, Numeric, Boolean
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-
-class Base(DeclarativeBase):
-    pass
+from sqlalchemy import Integer, String, Numeric
+from sqlalchemy.orm import Mapped, mapped_column
+from models.base import Base
 
 
 class Product(Base):
